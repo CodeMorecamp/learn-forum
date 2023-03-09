2cc4fdb999e7cd99136fcbdfb659542f14146899
CodeMorecamp <71688722+CodeMorecamp@users.noreply.github.com>
2023-03-09 13:42:49 +0100

    Backend

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require ('express')
+const cors = require('cors')
+const app = express()
+const PORT =4000
+
+//To be able use Express and CORS
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(cors())
+
+//render something to test the server
+app.get("./api", (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+})
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+})
+
+//hold all the existing users
+const users = [];
+
+//generate random string as an ID
+const generateID = () => Math.random().toString(36).substring(2,10);
+
+app.post("/api/register", async (req, res) => {
+    const [email, password, username] = req.body
+
+    //holds the ID
+    const id = generateID
+    //ensure there is no existing user with thesame credentials
+    const result = users.filter (
+        (user) => user.email=== email && user.password === password
+    )
+
+//if true
+     if (result.length === 0) {
+        const newUser = {id, email, password, username}
+        //adds the user to the database (array)
+        users.push(newUser)
+        //success messgae
+        return res.json ({
+            message: "Account created successfully"
+        })
+     } else {
+        res.json({
+            error_message: "User already exists"
+        })
+     }  
+    //log the credentials to the console
+    console.log({email, password, username, id})
+})
